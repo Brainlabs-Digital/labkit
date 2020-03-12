747796d849149de47bd99d9f22d94fce25579476
matthew-kaye <matthew@brainlabsdigital.com>
2020-03-12 13:47:01 +0000

    fix typo

--- documentation/views/SearchSelect.vue
+++ documentation/views/SearchSelect.vue
@@ -214,7 +214,7 @@ export default {
         changedItem(item) {
             this.selectedItem = item;
         },
-        inputHasError(item) {
+        inputHasError() {
             return true;
         },
         dataItems() {
